bd095ae5e29804c3e7cdc892767527af2a497885
info <info@quickstor.net>
2017-02-04 21:27:43 +0200

    fixing

--- initdisks.sh
+++ initdisks.sh
@@ -1,4 +1,6 @@
 cd /pace
+#### typeis : 1 for mirror... 0 for capacity stripe  ######
+typeis=`echo $@ | awk '{print $1}'`
 iscsimapping='/pacedata/iscsimapping';
 myhost=`hostname -s`
 runningpools='/pacedata/pools/runningpools';
@@ -26,10 +28,12 @@ while read -r  hostline ; do
   echo newdiskid=$newdiskid
   cat $runningpools | grep "$newdiskid" &>/dev/null
   if [ $? -ne 0 ]; then 
+   echo here $newdisk
    echo $host | grep $myhost &>/dev/null
    if [ $? -eq 0 ]; then
     hostdisk=("${hostdisk[@]}" "$host,$newdiskid");
    else
+    echo idledisk=$idledisk
     idledisk=("${idledisk[@]}" "$host,$newdiskid");
    fi
   fi
@@ -41,15 +45,26 @@ for localdisk in "${hostdisk[@]}"; do
  echo idledisk=${#idledisk[@]} localdisk=$localdisk 
  if [ ${#idledisk[@]} -eq 0 ]; then
    disk2=`echo ${localdisk} | awk -F',' '{print $2}'`
+ echo nextpool=$nextpool disk2=$disk2
    /sbin/zpool labelclear /dev/disk/by-id/${disk2};
    rm -rf /p${nextpool} &>/dev/null
-   /sbin/zpool create -o ashift=12 -o autoexpand=on -o autoreplace=on -f p${nextpool} ${disk2} ;
+   if [ $nextpool -ge 1 ]; then
+    if [ $typeis -eq 1 ]; then
+     olddisk=`cat $runningpools | grep "$myhost" | grep p1 | awk '{print $12}' | grep scsi`
+     echo olddisk=$olddisk
+   	/sbin/zpool attach -f  p1 $olddisk ${disk2} ;
+    else
+   	/sbin/zpool create -o ashift=12 -o autoexpand=on -o autoreplace=on -f p${nextpool} ${disk2} ;
+    fi
+   else
+   	/sbin/zpool create -o ashift=12 -o autoexpand=on -o autoreplace=on -f p${nextpool} ${disk2} ;
    /sbin/zfs set compression=lz4 p${nextpool}
    /sbin/zfs set atime=off p${nextpool}
    /sbin/zfs set xattr=sa p${nextpool}
    /sbin/zfs set redundant_metadata=most p${nextpool}
    /sbin/zfs set dedup=on p${nextpool}
    echo /sbin/zpool create p${nextpool} /dev/disk/by-id/scsi-${disk2} ;
+  fi
  else
   x=$((${#idledisk[@]}-1));
   if [ $x -ge 0 ]; then
