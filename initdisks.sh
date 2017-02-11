81086c7185b2e04bea141662b68176289d03f414
Abod <Abod@quickstor.net>
2017-02-11 19:26:44 +0200

    Abdo fixing

--- initdisks.sh
+++ initdisks.sh
@@ -3,42 +3,37 @@ cd /pace
 #### typeis : 1 for mirror... 0 for capacity stripe  ######
 typeis=`echo $@ | awk '{print $1}'`
 iscsimapping='/pacedata/iscsimapping';
+targethosts='/pacedata/iscsitargets';
 myhost=`hostname -s`
-runningpools='/pacedata/pools/runningpools';
+#runningpools='/pacedata/pools/runningpools';
+nhosts=`cat $targethosts | wc -l`
+if [ $nhosts -le 1 ]; then
+ /sbin/zpool import -a
+fi
 /sbin/zpool status > /pacedata/zpoolstatus
 /sbin/zpool import >>/pacedata/zpoolstatus
 runningpools='/pacedata/zpoolstatus';
 declare -a idledisk=();
 declare -a hostdisk=();
 declare -a runninghosts=(`cat $iscsimapping | grep -v notconnected | awk '{print $1}'`);
-phost='';
-for host in "${runninghosts[@]}" ; do
- echo $phost | grep $host
- if [ $? -ne 0 ]; then
-  phost=${phost}' '${host};
-  hostp=`ssh $host /sbin/zpool list -Hv ` 
-  hostp=${hostp}' '`ssh $host cat /TopStordata/hostname ` 
-  hostps=${hostps}' '${hostp};
- fi
-done
-#echo $hostps >> $runningpools
-sleep 2
 while read -r  hostline ; do
- host=`echo $hostline | awk '{print $1}'`;
- ls -l /dev/disk/by-path/ | grep "$host" &>/dev/null;
- if [ $? -eq 0 ]; then
-  devformatted=`echo $hostline | awk '{print $2}'`;
-  newdiskid=`echo $hostline | awk '{print $3}'`;
-  echo newdiskid=$newdiskid
-  cat $runningpools | grep "$newdiskid" &>/dev/null
-  if [ $? -ne 0 ]; then 
-   echo here $newdisk
-   echo $host | grep $myhost &>/dev/null
-   if [ $? -eq 0 ]; then
-    hostdisk=("${hostdisk[@]}" "$host,$newdiskid");
-   else
-    echo idledisk=$idledisk
-    idledisk=("${idledisk[@]}" "$host,$newdiskid");
+ host=`echo $hostline | grep -v notconnected | awk '{print $1}'`;
+ if [ ! -z "$host" ]; then
+  ls -l /dev/disk/by-path/ | grep "$host" &>/dev/null;
+  if [ $? -eq 0 ]; then
+   devformatted=`echo $hostline | awk '{print $2}'`;
+   newdiskid=`echo $hostline | awk '{print $3}'`;
+   echo newdiskid=$newdiskid
+   cat $runningpools | grep "$newdiskid" &>/dev/null
+   if [ $? -ne 0 ]; then 
+    echo here $newdisk
+    echo $host | grep $myhost &>/dev/null
+    if [ $? -eq 0 ]; then
+     hostdisk=("${hostdisk[@]}" "$host,$newdiskid");
+    else
+     echo idledisk=$idledisk
+     idledisk=("${idledisk[@]}" "$host,$newdiskid");
+    fi
    fi
   fi
  fi
