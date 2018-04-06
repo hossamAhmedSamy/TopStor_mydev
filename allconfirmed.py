c6f561e066f419aaf7e47d3d4193ef994c5dfa10
mezo <mezo@ldjfk.com>
2018-04-06 22:44:16 +0200

    fixing

--- allconfirmed.py
+++ allconfirmed.py
@@ -4,7 +4,7 @@ from ast import literal_eval as mtuple
 import socket
 
 myhost=socket.gethostname()
-cmdline=['/pace/etcdget.py','change','--prefix']
+cmdline=['/pace/etcdget.py','known','--prefix']
 result=subprocess.run(cmdline,stdout=subprocess.PIPE)
 change=str(result.stdout)[2:][:-3].split('\\n')
 if len(change) < 2:
@@ -16,7 +16,7 @@ confirmed=str(result.stdout)[2:][:-3].split('\\n')
 if len(confirmed) < 2:
  print('no confirm')
  exit()
-cmdline=['/pace/etcdget.py','known','--prefix']
+cmdline=['/pace/etcdget.py','change','--prefix']
 result=subprocess.run(cmdline,stdout=subprocess.PIPE)
 known=str(result.stdout)[2:][:-3].split('\\n')
 if len(known) < 2:
