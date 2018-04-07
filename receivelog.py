20159a6dc7d206a6aa3c5711b996b5dea957bed4
mezo <mezo@ldjfk.com>
2018-04-07 13:38:21 +0200

    fixing

--- receivelog.py
+++ receivelog.py
@@ -7,33 +7,45 @@ from os.path import isfile, join
 
 myhost=socket.gethostname()
 fpath='/var/www/html/des20/Data/'
-
+#### exit if found me in confirmed
+cmdline=['/pace/etcdget.py','broadcast/confirmed','--prefix']
+result=subprocess.run(cmdline,stdout=subprocess.PIPE)
+conf=str(result.stdout).replace('broadcast/confirmed/','')[2:][:-3].split('\\n')
+if myhost in str(conf):
+ exit()
+#############
 cmdline=['/pace/etcdget.py','known','--prefix']
 result=subprocess.run(cmdline,stdout=subprocess.PIPE)
 known=str(result.stdout).replace('known/','')[2:][:-3].split('\\n')
 cmdline=['/pace/etcdget.py','broadcast/response','--prefix']
 result=subprocess.run(cmdline,stdout=subprocess.PIPE)
 broad=str(result.stdout).replace('broadcast/response/','')[2:][:-3].split('\\n')
-######### if no broadcast response from othrs
-isbroad=0
-if broad==[''] or all(myhost in mtuple(x.replace('\\',''))[0] for x in broad):
- onlyfiles = [f for f in listdir(fpath) if isfile(join(fpath, f)) and "TopStor.log." in f]
- if onlyfiles==[''] or len(known) > len(onlyfiles):
-  cmdline=['/pace/etcdput.py','broadcast/request/'+myhost, '1']
-  result=subprocess.run(cmdline,stdout=subprocess.PIPE)
-  exit()
- mini=99999999999999999
- for f in onlyfiles:
-  with open(fpath+f) as ff:
-  
-   last=int(str(ff.readlines()[-1]).split(' ')[5][1:][:-3])
-   if last < mini: 
-    mini=last
- cmdline=['/pace/etcdput.py','broadcast/request/'+myhost, str(mini+1)]
+######## prepare times from files###########
+onlyfiles = [f for f in listdir(fpath) if isfile(join(fpath, f)) and "TopStor.log." in f]
+if onlyfiles==[''] or len(known) > len(onlyfiles):
+ cmdline=['/pace/etcdput.py','broadcast/request/'+myhost, '1']
  result=subprocess.run(cmdline,stdout=subprocess.PIPE)
  exit()
+preptimes=[]
+mini=99999999999999999
+for f in onlyfiles:
+ with open(fpath+f) as ff:
+  last=int(str(ff.readlines()[-1]).split(' ')[5])
+  if last < mini: 
+   mini=last
+  mini+=1
+  preptimes.append((f,last))
+#################### get all responses
+cmdline=['/pace/etcdget.py','broadcast/response','--prefix']
+result=subprocess.run(cmdline,stdout=subprocess.PIPE)
+broad=str(result.stdout).replace('broadcast/response/','')[2:][:-3].split('\\n')
 cmdline=[]
 counter=0
+if broad==['']:
+ cmdline=['/pace/etcdput.py','broadcast/request/'+myhost, str(mini) ]
+ result=subprocess.run(cmdline,stdout=subprocess.PIPE)
+ exit()
+confirmed=False
 for k in broad:
  k=k.replace('\\','')
  k=mtuple(k)
@@ -41,13 +53,32 @@ for k in broad:
  if host==myhost:
   continue
  cmdline=[]
+ cmdline2=[]
  counter=0
+############ update responses to file only if last time (in file) is less than first time in response
+ if host in str(preptimes): 
+  for s in preptimes:
+   if host in s[0]:
+    last=s[1]
+ else:
+   last=1
+ first=9999999999999999
  with  open('/var/www/html/des20/Data/TopStor.log.'+host,'at') as f:
   a=str(k[1])
   h=mtuple(a)
   for m in h:
-   ss=str(m).replace('[','').replace(']','')+'\n'
-   cmdline.append(ss)
-  f.writelines(cmdline[::-1])
+   ss=str(m).replace('[','').replace(']','').replace("'",'')
+   ssfirst=int(ss.split(' ')[5])
+   if first > ssfirst:
+     first=ssfirst
+   cmdline.append(ss.replace(',','')+'\n')
+  if first > last:
+   f.writelines(cmdline[::-1])
+   confirmed=True;
+if confirmed:
  cmdline=['/pace/etcdput.py','broadcast/confirmed/'+host+'/'+myhost, 'done']
  result=subprocess.run(cmdline,stdout=subprocess.PIPE)
+else:
+ cmdline=['/pace/etcdput.py','broadcast/request/'+myhost, str(mini) ]
+ result=subprocess.run(cmdline,stdout=subprocess.PIPE)
+ print('no udpate')
