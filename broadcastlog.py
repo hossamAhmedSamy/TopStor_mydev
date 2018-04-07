7da6c3726cf5c01da644605d7c0b6a45e44e3aa8
mezo <mezo@ldjfk.com>
2018-04-07 14:08:25 +0200

    fixing

--- broadcastlog.py
+++ broadcastlog.py
@@ -4,23 +4,23 @@ from ast import literal_eval as mtuple
 import socket
 
 myhost=socket.gethostname()
-cmdline=['/pace/etcdget.py','known','--prefix']
-result=subprocess.run(cmdline,stdout=subprocess.PIPE)
-known=str(result.stdout).replace('known/','')[2:][:-3].split('\\n')
-if known==['']:
- print('no partners')
- exit();
-cmdline=['/pace/etcdget.py','broadcast/response/'+myhost,'--prefix']
-result=subprocess.run(cmdline,stdout=subprocess.PIPE)
-respo=str(result.stdout).replace('broadcast/response/'+myhost,'')[2:][:-3].split('\\n')
-if respo != ['']:
- exit()
+#cmdline=['/pace/etcdget.py','known','--prefix']
+#result=subprocess.run(cmdline,stdout=subprocess.PIPE)
+#known=str(result.stdout).replace('known/','')[2:][:-3].split('\\n')
+#if known==['']:
+# print('no partners')
+# exit();
 cmdline=['/pace/etcdget.py','broadcast/confirmed','--prefix']
 result=subprocess.run(cmdline,stdout=subprocess.PIPE)
 conf=str(result.stdout).replace('broadcast/confirmed/','')[2:][:-3].split('\\n')
 if conf != ['']:
  cmdline=['/pace/etcddel.py','broadcast','--prefix']
  result=subprocess.run(cmdline,stdout=subprocess.PIPE)
+ exit()
+cmdline=['/pace/etcdget.py','broadcast/response/'+myhost,'--prefix']
+result=subprocess.run(cmdline,stdout=subprocess.PIPE)
+respo=str(result.stdout).replace('broadcast/response/'+myhost,'')[2:][:-3].split('\\n')
+if respo != ['']:
  exit()
   
 cmdline=['/pace/etcdget.py','broadcast/request','--prefix']
@@ -44,11 +44,10 @@ with open('/var/www/html/des20/Data/TopStor.log','rt') as f:
  revf=f.readlines()[::-1]
  for line in revf:
   line=str(line).split(' ')
-  if float(start[1]) <= float(line[5]):
+  if int(start[1]) <= int(line[5]):
    counter+=1
    line[5]=line[5].replace('\n','')
    broad.append(line)
- cmdline=['/pace/etcdput.py','broadcast/response/'+myhost,str(broad)]
- result=subprocess.run(cmdline,stdout=subprocess.PIPE)
- print(line)
-print('counter=',counter)
+ if counter > 0:
+  cmdline=['/pace/etcdput.py','broadcast/response/'+myhost,str(broad)]
+  result=subprocess.run(cmdline,stdout=subprocess.PIPE)
