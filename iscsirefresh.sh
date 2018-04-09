c728ac9f35be3b2e451f6f5b3c86a0786d04a6c3
mezo <mezo@mezo.com>
2018-04-10 00:13:54 +0200

    fixing

--- iscsirefresh.sh
+++ iscsirefresh.sh
@@ -1,23 +1,28 @@
 cd /pace
 iscsimapping='/pacedata/iscsimapping';
 iscsitargets='/pacedata/iscsitargets';
+declare -a iscsitargets=(`ETCDCTL_API=3 ./iscsiclients.py`);
+
 /sbin/iscsiadm -m session --rescan &>/dev/null
 needrescan=0;
 myhost=`hostname -s`
-while read -r  hostline ; do
- host=`echo $hostline | awk '{print $2}'`
- echo $myhost | grep $host
+for hostline in "${iscsitargets[@]}"
+do
+ echo $myhost | grep $hostline
  if [ $? -eq 0 ]; then
   host='127.0.0.1'
+ else
+  host=` ETCDCTL_API=3 ./etcdgetip.py $hostline`
  fi
+ echo hihi
  ping -c 1 -W 1 $host &>/dev/null
  if [ $? -eq 0 ]; then
   needrescan=1;
   hostpath=`ls /var/lib/iscsi/nodes/ | grep "$host"`;
-  echo cat $iscsimapping  echo $host  grep notconnected 
+  echo hi cat $iscsimapping  echo $host  grep notconnected 
   cat $iscsimapping |  grep "$host" | grep notconnected &>/dev/null
   if [ $? -eq 0 ]; then
-   echo here
+   echo herehihi
    #rm -rf /var/lib/iscsi/nodes/send_targets/$host* &>/dev/null
    hostiqn=`/sbin/iscsiadm -m discovery --portal $host --type sendtargets | awk '{print $2}'`
    /sbin/iscsiadm -m node --targetname $hostiqn --portal $host -u
@@ -34,7 +39,7 @@ while read -r  hostline ; do
   #rm -rf /var/lib/iscsi/nodes/iqn.2016-03.com.${host}:t1 
   #rm -rf /var/lib/iscsi/nodes/${host}* &>/dev/null
  fi
-done < $iscsitargets
+done
 if [ $needrescan -eq 1 ]; then
 # /sbin/iscsiadm -m session --rescan &>/dev/null
  sleep 1; 
