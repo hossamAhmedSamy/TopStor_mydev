1c143b47b6435807cc103d6638d991ab92059ea7
dhdf <dhdf@#dklfja;asd>
2018-05-08 14:49:51 +0200

    fixing

--- iscsiwatchdog.sh
+++ iscsiwatchdog.sh
@@ -2,6 +2,8 @@
 systemctl status etcd 
 if [ $? -eq 0 ];
 then
+ systemctl restart target
+ systemctl restart iscsi
  sh /pace/iscsirefresh.sh
  sh /pace/listingtargets.sh
  sh /pace/addtargetdisks.sh
