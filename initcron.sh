a98f80ecb36d7bbe59c50483a81054aa5c8c8392
dhdf <dhdf@#dklfja;asd>
2018-05-08 17:31:35 +0200

    fixing

--- initcron.sh
+++ initcron.sh
@@ -7,7 +7,6 @@ myip=`cat /etc/hosts | grep $myhost | head -1 | awk '{print $1}'`
 #sh /pace/iscsirefresh.sh
 #sh /pace/listingtargets.sh
 #sh /pace/addtargetdisks.sh
-systemctl start pacemaker
 sleep 120 
 /sbin/pcs resource delete --force IPinit
 /sbin/ip addr del 10.11.11.254/24 dev enp0s8 
