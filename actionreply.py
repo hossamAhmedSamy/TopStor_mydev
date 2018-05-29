ddd40ed805c20e1e779c3b0d55c4dc86dc98fcf3
dhdf <dhdf@#dklfja;asd>
2018-05-29 12:31:08 +0200

    fixing

--- actionreply.py
+++ actionreply.py
@@ -5,18 +5,12 @@ def do(body):
  z=[]
  with open('/root/recv','a') as f:
   f.write('Recevied a reply:'+str(body))
- exit()
  r=mtuple((body[2:][:-1]))
 # print("receved from",r["host"],' request for: ',r["req"])
 # mylist=get('run',r["req"])
  if r["req"]=='user':
-  with open('/etc/passwd') as f:
-   revf=f.readlines()
-   for line in revf:
-    if 'TopStor' in line:
-     l=line.split(':')
-     ll=line.split('TopStor')[1].split(':/')[0]
-     z.append((l[0],l[2],ll)) 
+  with open('/root/recv','a') as f:
+   f.write('User list in reply:'+str(r["reply"]))
  print(z)  
 # if r["req"]=='user':
 if __name__=='__main__':
