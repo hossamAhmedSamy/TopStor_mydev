ab6c2c5d73c68e8441f7d1172e35e0f625dc1de6
m22 <m22@dlafjkd.comd>
2018-10-07 15:10:20 +0200

    fixing

--- setdataip.py
+++ setdataip.py
@@ -6,7 +6,8 @@ import json
 def setdataip(*args):
  nslist=get('dataip','--prefix')
  for x in nslist:
-  name=x[0].replace('/','')
+  name=x[0].replace('/dataip','')
+  name=name.replace('/','')
   params=x[1].split('/')
   cmdline='/sbin/pcs resource create '+name+' ocf:heartbeat:IPaddr2 nic='+params[2]+' ip='+params[0]+' cidr_netmask='+params[1]+' op monitor on-fail=restart'
   subprocess.run(cmdline.split(),stdout=subprocess.PIPE)
