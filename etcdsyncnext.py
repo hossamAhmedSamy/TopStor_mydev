f1fa742211a95e0754eb63b70ed5a3245f8f1415
dkfj <skdlfjl@sdlfj>
2018-10-13 23:15:57 +0200

    sldfkj

---
+++ etcdsyncnext.py
@@ -0,0 +1,23 @@
+#!/bin/python3.6
+import subprocess,sys
+from ast import literal_eval as mtuple
+from etcdget import etcdget as get
+
+
+thehost=sys.argv[1]
+key=sys.argv[2]
+tokey=sys.argv[3]
+result=get(key,'--prefix')
+print("the result",result)
+if result==[]:
+ print('-1')
+ exit(1)
+for item in result:
+ x=item[0].replace('/','')
+ x=x.replace(str(key),'')
+ if x =='':
+   adding='';
+ else:
+   adding='/'+x
+ cmdline=['/pace/etcdputlocal.py',thehost, tokey+adding, item[1]]
+ result=subprocess.run(cmdline,stdout=subprocess.PIPE)
--- syncnext.sh
+++ syncnext.sh
@@ -4,4 +4,4 @@ cd /TopStor/
 theone=` echo $@ | awk '{print $1}'`;
 thetwo=` echo $@ | awk '{print $2}'`;
 myip=`/sbin/pcs resource show CC | grep Attributes | awk '{print $2}' | awk -F'=' '{print $2}'`
-/pace/etcdsync.py $myip $theone $thetwo 2>/dev/null
+./etcdsyncnext.py $myip $theone $thetwo 2>/dev/null
