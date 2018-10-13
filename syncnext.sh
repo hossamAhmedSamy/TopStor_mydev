6067f2de3e0778b6938473096c9fb2a4191816b8
m22 <m22@dlafjkd.comd>
2018-10-13 22:52:45 +0200

    dsalfjk

---
+++ syncnext.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+export ETCDCTL_API=3
+cd /TopStor/
+theone=` echo $@ | awk '{print $1}'`;
+thetwo=` echo $@ | awk '{print $2}'`;
+myip=`/sbin/pcs resource show CC | grep Attributes | awk '{print $2}' | awk -F'=' '{print $2}'`
+/pace/etcdsync.py $myip $theone $thetwo 2>/dev/null
