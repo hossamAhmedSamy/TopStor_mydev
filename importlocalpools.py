324d77b5af2aa7eb50c1c8fa328f516c633959b9
dkljf <dlksjf@dlafjk>
2018-11-06 23:28:41 +0200

    dfal;kj

--- importlocalpools.py
+++ importlocalpools.py
@@ -6,8 +6,8 @@ def importpools(*args):
  with open('/TopStordata/forlocalpools') as f:
   for line in f:
    pool=mtuple(line)[0].split('/')[1]
-     cmdline=['/TopStor/Zpool2 import -c /TopStordata/'+pool+' -am']
-     result=subprocess.run(cmdline,stdout=subprocess.PIPE)
+   cmdline=['/TopStor/Zpool2 import -c /TopStordata/'+pool+' -am']
+   result=subprocess.run(cmdline,stdout=subprocess.PIPE)
    print(pool)
  return
 
