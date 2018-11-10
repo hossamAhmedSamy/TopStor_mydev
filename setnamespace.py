40a7be473575b79b5ba80b310be2544baa6f72c6
m22 <m22@dlafjkd.comd>
2018-11-10 15:17:50 +0200

    fixing'

--- setnamespace.py
+++ setnamespace.py
@@ -6,7 +6,7 @@ import json
 def setnamespace(*args):
  nslist=get('namespace','--prefix')
  if 'mgmtip' not in str(nslist):
-  put('namespace/mgmtip','10.11.11.252')
+  put('namespace/mgmtip','192.168.43.7/24')
   nslist=get('namespace','--prefix')
  ns=(x for x in nslist)
  for arg in args:
@@ -14,7 +14,7 @@ def setnamespace(*args):
    nsn=next(ns)
   except: 
    return
-  cmdline='/sbin/pcs resource create '+nsn[0].replace('namespace/','')+' ocf:heartbeat:IPaddr2 nic='+arg+' ip='+nsn[1]+' cidr_netmask=24 op monitor on-fail=restart'
+  cmdline='/sbin/pcs resource create '+nsn[0].replace('namespace/','')+' ocf:heartbeat:IPaddr2 nic='+arg+' ip='+nsn[1].split('/')[0]+' cidr_netmask='+nsn[1].split('/')[1]+' op monitor on-fail=restart'
   subprocess.run(cmdline.split(),stdout=subprocess.PIPE)
   cmdline='/sbin/pcs resource group add namespaces '+nsn[0].replace('namespace/','')
   subprocess.run(cmdline.split(),stdout=subprocess.PIPE)
