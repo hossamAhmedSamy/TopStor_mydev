49cc11cfe604d2c95903930529db65e87be9378c
dlfjkd <dkfaj;l@sldkjf.com>
2018-12-01 18:36:46 +0200

    dfajl;k

--- privthis.sh
+++ privthis.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 export ETCDCTL_API=3
+cd /TopStor
 userpriv='/var/www/html/des20/Data/userpriv.txt';
 userreq=` echo $@ | awk '{print $2}'`;
 modpriv=` echo $@ | awk '{print $1}'`;
@@ -11,5 +12,18 @@ then
 else
  userinfo=`/TopStor/etcdget.py usersinfo/$userreq | awk -F"$modpriv" '{print $2}'`
  priv=`echo $userinfo | cut -c 2- |  awk -F'/' '{print $1}'`
- echo $priv;
+ echo $priv | grep 'true' >/dev/null
+ if [ $? -eq 0 ];
+ then
+  ./UnixChkUser2 $userreq 'cheking'
+  x=`./etcdget.py logged/$userreq | wc -c`
+  if [ x -gt 5 ];
+  then
+   echo $priv;
+  else
+   echo 'false'
+  fi
+ else
+  echo 'false'
+ fi
 fi
