1b54894dda79e980c428d03bdf90e875f45187b8
root <dklfaj@dflkjf.com>
2019-01-05 02:48:00 +0200

    fiing

--- etcdsync.py
+++ etcdsync.py
@@ -10,23 +10,18 @@ thehost=sys.argv[1]
 key=sys.argv[2]
 tokey=sys.argv[3]
 print('thehost',thehost,tokey)
-slash=1
-mylist=get(key+'/','--prefix')
-if len(mylist) == 0:
- mylist=get(key,'--prefix')
- slash=0
-if slash==0:
- dellocal(thehost,tokey,'--prefix')
-else:
- dellocal(thehost,tokey+'/','--prefix')
+mylist=get(key,'--prefix')
+dellocal(thehost,tokey,'--prefix')
 
 print('mylist:',mylist)
 if '-1' in mylist:
  print('-1')
  exit()
 for item in mylist:
- if slash==1:
-  putlocal(thehost, tokey+'/'+item[0].split('/')[1], item[1])
- else:
-  putlocal(thehost, tokey,item[1])
+ moditem=""
+ restitem=""
+ if '/' in item[0]:
+  moditem=item[0].split('/')[0]
+  restitem='/'+item[0].replace(moditem+'/','')
+ putlocal(thehost, tokey+restitem, item[1])
  
