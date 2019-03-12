24e28d45a06c5c5bd9880b2c66ed59bb2624e92b
dklafj <dklafj>
2019-03-12 20:33:50 +0200

    dafj;

--- addhost.py
+++ addhost.py
@@ -7,19 +7,19 @@ import socket, sys, subprocess
 def addhost(*args):
  myhost=socket.gethostname()
  allready=get('ready','--prefix')
- with open('/TopStor/grafana.files/hosts','w') as fw:
-  with open('/TopStor/grafana.files/hosts.orig','r') as fr:
-   with open('/TopStor/prometheus.files/hosts','w') as pfw:
-    with open('/TopStor/prometheus.files/hosts.orig','r') as pfr:
-     with open('/TopStor/prometheus.files/prometheus.yml','w') as ppfw:
-      with open('/TopStor/prometheus.files/prometheus.yml.orig','r') as ppfr:
+ with open('/TopStordata/grafana.files/hosts','w') as fw:
+  with open('/TopStordata/grafana.files/hosts.orig','r') as fr:
+   with open('/TopStordata/prometheus.files/hosts','w') as pfw:
+    with open('/TopStordata/prometheus.files/hosts.orig','r') as pfr:
+     with open('/TopStordata/prometheus.files/prometheus.yml','w') as ppfw:
+      with open('/TopStordata/prometheus.files/prometheus.yml.orig','r') as ppfr:
        fw.write(fr.read())
        pfw.write(pfr.read())
        ppfw.write(ppfr.read())
        for ready in allready:
         fw.write('\n'+ready[1]+' '+ready[0].replace('ready/',''))
         pfw.write('\n'+ready[1]+' '+ready[0].replace('ready/',''))
-        ppfw.write("       - '"+ready[0].replace('ready/','')+":9090'"+'\n')
+        ppfw.write("       - '{"+ready[0].replace('ready/','')+":9090}'"+'\n')
  cmdline=['/bin/docker','restart','prom']
  subprocess.run(cmdline,stdout=subprocess.PIPE)
  cmdline=['/bin/docker','restart','grafana']
