86dafc63242c9d3532ac3caf083ab3482a9ffaad
dkfjl@dljf <dkfjl@dljf>
2019-04-29 17:00:44 +0200

    dfaj;k

--- putzpoolimport.py
+++ putzpoolimport.py
@@ -1,20 +1,17 @@
 #!/bin/python3.6
 import subprocess
+from etcdget import etcdget as get
 
 def putzpoolimport():
  cmdline='/sbin/zpool import'
  result=subprocess.run(cmdline.split(),stdout=subprocess.PIPE).stdout
  sty=str(result)[2:][:-3].replace('\\t','').split('\\n')
+ sty=get('activepool','--prefix')
  zdict={}
  zpool=[]
  for a in sty:
-  print('aaaaaa',a)
-  b=a.split()
-  if len(b) > 1:
-   if "pdhc" in b[0] and  'pool:' not in str(b):
-    #if 'DEG' in b[1] or 'ONLI' in b[1]:
-     zdict={ 'name':b[0], 'status':b[1]}
-     zpool.append(zdict)
+  zdict={ 'name':a[0].replace('activepool/',''), 'status':a[1]}
+  zpool.append(zdict)
  print('pools',zpool)
  return zpool 
 
