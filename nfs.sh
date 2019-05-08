ff7f536478961ea7417f9f406fea095ceaf9b1d8
dkslfj <dkslfj>
2019-05-08 21:29:48 +0200

    ejdkfaj

--- nfs.sh
+++ nfs.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
+enpdev='enp0s8'
 pool=`echo $@ | awk '{print $1}'`
 vol=`echo $@ | awk '{print $2}'`
-docker run -v /pdhcp1807130007/nefo:/pdhcp1807130007/nefo -v /etc/exports.nefo:/etc/exports:ro --cap-add SYS_ADMIN -p 2049:2049  -p 2049:2049/udp  -p 32765:32765 -p 32765:32765/udp -p 111:111 -p 111:111/udp -p 32767:32767 -p 32767:32767/udp --name nfs 10.11.11.124:5000/nfs
+ipaddr=`echo $@ | awk '{print $3}'`
+ipsubnet=`echo $@ | awk '{print $4}'`
+echo $@ > /root/nfsparam
+/sbin/pcs resource delete --force ip-$vol  2>/dev/null
+/sbin/pcs resource create ip-$vol ocf:heartbeat:IPaddr2 ip=$ipaddr nic=$enpdev cidr_netmask=$ipsubnet op monitor interval=5s on-fail=restart
+/sbin/pcs resource group add ip-all ip-$vol
+docker stop nfs-$vol
+docker container rm nfs-$vol
+docker run -d -v /$pool/$vol:/$pool/$vol -v /etc/exports.$vol:/etc/exports:ro --cap-add SYS_ADMIN -p $ipaddr:2049:2049  -p $ipaddr:2049:2049/udp  -p $ipaddr:32765:32765 -p $ipaddr:32765:32765/udp -p $ipaddr:111:111 -p $ipaddr:111:111/udp -p $ipaddr:32767:32767 -p $ipaddr:32767:32767/udp --name nfs-$vol 10.11.11.124:5000/nfs
