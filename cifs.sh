62296f7e23951e382cc593ae86f1a43fa6334c63
dkslfj <dkslfj>
2019-05-16 10:15:35 +0200

    fixing

--- cifs.sh
+++ cifs.sh
@@ -5,35 +5,40 @@ pool=`echo $@ | awk '{print $1}'`
 vol=`echo $@ | awk '{print $2}'`
 ipaddr=`echo $@ | awk '{print $3}'`
 ipsubnet=`echo $@ | awk '{print $4}'`
+echo $@ > /root/cifsparam
 rightvol=`/pace/etcdget.py ipaddr/$ipaddr`
-echo $rightvol=
 if [ $rightvol -eq '-1' ];
 then
  /pace/etcdput.py ipaddr/$ipaddr 1/$vol
  /pace/broadcasttolocal.py ipaddr/$ipaddr 1/$vol 
- echo $@ > /root/nfsparam
- docker stop nfs-$pool-$vol
- docker container rm nfs-$pool-$vol
- yes | cp /etc/{passwd,group,shadow} /opt/passwds
- cp /TopStordata/exports.${vol} /TopStordata/exports.$ipaddr
+ docker stop cifs-$pool-$vol
+ docker container rm cifs-$pool-$vol
  /sbin/pcs resource delete --force ip-$pool-$vol  2>/dev/null
  /sbin/pcs resource create ip-$pool-$vol ocf:heartbeat:IPaddr2 ip=$ipaddr nic=$enpdev cidr_netmask=$ipsubnet op monitor interval=5s on-fail=restart
  /sbin/pcs resource group add ip-all ip-$pool-$vol
- docker run -d -v /$pool:/$pool:rw -v /TopStordata/exports.$ipaddr:/etc/exports:ro \
-  --cap-add SYS_ADMIN -p $ipaddr:2049:2049  -p $ipaddr:2049:2049/udp \
-  -p $ipaddr:32765:32765 -p $ipaddr:32765:32765/udp \
-  -p $ipaddr:111:111 -p $ipaddr:111:111/udp \
-  -p $ipaddr:32767:32767 -p $ipaddr:32767:32767/udp \
+ yes | cp /etc/{passwd,group,shadow} /opt/passwds
+ cp /TopStor/smb.conf /TopStordata/smb.$ipaddr
+ cat /TopStordata/smb.${vol}>> /TopStordata/smb.$ipaddr
+ docker run -d -v /$pool:/$pool:rw --privileged \
+  -e "HOSTIP=$ipaddr"  \
+  -p $ipaddr:135:135 \
+  -p $ipaddr:137-138:137-138/udp \
+  -p $ipaddr:139:139 \
+  -p $ipaddr:445:445 \
+  -v /etc/localtime:/etc/localtime:ro \
+  -v /TopStordata/smb.${ipaddr}:/config/smb.conf:rw \
   -v /opt/passwds/passwd:/etc/passwd:rw \
   -v /opt/passwds/group:/etc/group:rw \
   -v /opt/passwds/shadow:/etc/shadow:rw \
-  --name nfs-$pool-$vol 10.11.11.124:5000/nfs
+  -v /var/lib/samba/private:/var/lib/samba/private:rw \
+  --name cifs-$pool-$vol 10.11.11.124:5000/smb
 else
  count=`echo $rightvol | awk -F'/' '{print $1}'`
  origvol=`echo $rightvol | awk -F'/' '{print $2}'`
  newcount=$((count+1))
  /pace/etcdput.py ipaddr/$ipaddr $newcount/$origvol
  /pace/broadcasttolocal.py ipaddr/$ipaddr $newcount/$origvol 
- cat /TopStordata/exports.${vol} >> /TopStordata/exports.$ipaddr
- docker exec -it nfs-$pool-$origvol exportfs -ra 
+ cat /TopStordata/smb.${vol} >> /TopStordata/smb.$ipaddr
+ docker exec -it cifs-$pool-$origvol smbcontrol reload-config
 fi
+
---
+++ cifsAD.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+enpdev='enp0s8'
+pool=`echo $@ | awk '{print $1}'`
+vol=`echo $@ | awk '{print $2}'`
+ipaddr=`echo $@ | awk '{print $3}'`
+ipsubnet=`echo $@ | awk '{print $4}'`
+echo $@ > /root/cifsparam
+docker stop cifs-$pool-$vol
+docker container rm cifs-$pool-$vol
+/sbin/pcs resource delete --force ip-$pool-$vol  2>/dev/null
+/sbin/pcs resource create ip-$pool-$vol ocf:heartbeat:IPaddr2 ip=$ipaddr nic=$enpdev cidr_netmask=$ipsubnet op monitor interval=5s on-fail=restart
+/sbin/pcs resource group add ip-all ip-$pool-$vol
+docker run -d -v /$pool/$vol:/$pool/$vol --privileged \
+ -e "HOSTIP=$ipaddr"  \
+ -p $ipaddr:53:53 \
+ -p $ipaddr:53:53/udp \
+ -p $ipaddr:88:88 \
+ -p $ipaddr:88:88/udp \
+ -p $ipaddr:135:135 \
+ -p $ipaddr:137-138:137-138/udp \
+ -p $ipaddr:139:139 \
+ -p $ipaddr:389:389 \
+ -p $ipaddr:389:389/udp \
+ -p $ipaddr:445:445 \
+ -p $ipaddr:464:464 \
+ -p $ipaddr:464:464/udp \
+ -p $ipaddr:636:636 \
+ -p $ipaddr:1024-1044:1024-1044 \
+ -p $ipaddr:3268-3269:3268-3269 \
+ -v /etc/localtime:/etc/localtime:ro \
+ -v /TopStordata/smb${name}.conf:/etc/samba/external \
+ -v /TopStordata/data${name}:/var/lib/samba \
+ --name cifs-$pool-$vol 10.11.11.124:5000/samba
