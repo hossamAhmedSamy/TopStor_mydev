4e4217843dc0edfb84fde6d481e72bf943678acc
dkafjdk.sdfj <dkjafl@sldkfj.com>
2020-05-25 03:47:44 +0200

    fixing

--- VolumeCIFSupdate.sh
+++ VolumeCIFSupdate.sh
@@ -1,6 +1,6 @@
 rm -rf /etc/passwd
 rm -rf /etc/group
 rm -rf /etc/shadow
-ln -s /hosetc/passwd /etc/passwd
-ln -s /hosetc/passwd /etc/group
-ln -s /hosetc/passwd /etc/shadow
+ln -s /hostetc/passwd /etc/
+ln -s /hostetc/group  /etc/
+ln -s /hostetc/shadow  /etc/
