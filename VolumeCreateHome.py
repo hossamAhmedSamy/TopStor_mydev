e17f99073e77b96831caade028869f84ec29b3a1
dkdfj <dkajf@sdkfj.com>
2020-06-20 13:46:53 +0300

    fixing

--- VolumeCreateHome.py
+++ VolumeCreateHome.py
@@ -6,6 +6,8 @@ from ast import literal_eval as mtuple
 from socket import gethostname as hostname
 from sendhost import sendhost
 def send(*bargs):
+ cmdline=['/TopStor/queuethis.sh','VolumeCreateHome.py','running',bargs[-1]]
+ result=subprocess.run(cmdline,stdout=subprocess.PIPE)
  if(len(bargs) < 3):
   args=bargs[0].split()
  else:
@@ -17,7 +19,7 @@ def send(*bargs):
  z=[]
  with open('/root/VolumeCreate','w') as f:
   f.write('pool='+pool+'\n')
- owner=args[-1]
+ owner=args[-2]
  with open('/root/VolumeCreate','a') as f:
   f.write('owner='+owner+'\n')
  myhost=hostname()
@@ -27,15 +29,19 @@ def send(*bargs):
  if ownerip[0]== -1:
   ownerip=get('known',owner)
   if ownerip[0]== -1:
+   cmdline=['/TopStor/queuethis.sh','VolumeCreateHome.py','canceled',bargs[-1]]
+   result=subprocess.run(cmdline,stdout=subprocess.PIPE)
    return 3
  z=['/TopStor/pump.sh','VolumeCreateHome']
- for arg in args[:-1]:
+ for arg in args[:-2]:
   z.append(arg)
  msg={'req': 'VolumeCreate', 'reply':z}
  with open('/root/VolumeCreate','a') as f:
   f.write('myhost='+ownerip[0][1]+' '+myhost+' '+str(z)+'\n')
  sendhost(ownerip[0][1], str(msg),'recvreply',myhost)
- return 1
+ cmdline=['/TopStor/queuethis.sh','VolumeCreateHome.py','finished',bargs[-1]]
+ result=subprocess.run(cmdline,stdout=subprocess.PIPE)
+ return
 
 if __name__=='__main__':
  send(*sys.argv[1:])
