739471f6af6d1f637fc51de8ce0a65b5374f2fd6
dksafj <yodusaf@sljdf>
2020-07-25 06:14:48 +0300

    fixing

--- Evacuate.py
+++ Evacuate.py
@@ -1,54 +1,12 @@
 #!/bin/python3.6
 import subprocess,sys, datetime
 import json
-from etcdget import etcdget as get
 from etcdput import etcdput as put 
-from etcddel import etcddel as deli 
-from ast import literal_eval as mtuple
-from socket import gethostname as hostname
-from broadcast import broadcast as broadcast 
-from sendhost import sendhost
-from time import sleep
 import logmsg
-def sendlog(*args):
- z=[]
- knowns=[]
- myhost=hostname()
+def do(*args):
  logmsg.sendlog('Evacuaest01','info',args[-1],args[-2])
- broadcast('RemoveTargets', args[-2])
- z=['/TopStor/Evacuatelocal.py']
- with open('/root/evacuate','w') as f:
-  f.write('bargs'+str(args)+'\n')
- for arg in args:
-  z.append(arg)
- hostip=get('ActivePartners/'+args[-2])
- losts=get('lost','--prefix')
- knowns=get('knwon','--prefix')
- readys=get('ready','--prefix')
- theleader=get('leader','--prefix')[0]
- nextleader=theleader[0].replace('leader/','') 
- leader=theleader[1]
- if args[-2] in str(nextleader):
-  isleader=1
-  nextleader=get('nextlead')[0].split('/')[0]
- if args[-2] in str(readys):
-  z.append(leader)
-  z.append(hostip)
-  msg={'req': 'Evacuate', 'reply':z}
-  sendhost(hostip[0], str(msg),'recvreply',myhost)
- isleader=1
- while isleader:
-  print('still leader')
-  sleep(2)
-  leader=get('leader','--prefix')
-  if nextleader in str(leader):
-   isleader=0
- sleep(2)
- frstnode=get('frstnode')
- newnode=frstnode[0].replace('/'+args[-2],'').replace(args[-2]+'/','')
- put('frstnode',newnode)
- deli('',args[-2])
+ put('toremovenode/'+args[-2],'start')
  put("tosync","yes")
 
 if __name__=='__main__':
- sendlog(*sys.argv[1:])
+ do(*sys.argv[1:])
--- Evacuatelocal.py
+++ Evacuatelocal.py
@@ -1,33 +1,47 @@
 #!/bin/python3.6
-import subprocess,sys, datetime,socket
-import json
-from etcddellocal import etcddel as delilocal
-from etcdputlocal import etcdput as putlocal 
+import subprocess,sys, datetime
+from etcdput import etcdput as put 
+from etcdget import etcdget as get 
+from etcddel import etcddel as deli 
+from socket import gethostname as hostname
 import logmsg
 def setall(*bargs):
- arg=bargs
- myhost=bargs[-4]
- name=bargs[-4]
- leader=bargs[-2]
- myip=bargs[-1]
- cmdline=['/TopStor/queuethis.sh','Evacuate.py','running',bargs[-1]]
- result=subprocess.run(cmdline,stdout=subprocess.PIPE)
- with open('/root/evacuatelocal','w') as f:
-  f.write('bargs'+str(bargs)+'\n')
- if myip in str(leader):
-  with open('/root/evacuatelocal','a') as f:
-   f.write('iamleader '+myip+' '+name+'\n')
-  cmdline=['/TopStor/Converttolocal.sh',myip]
-  result=subprocess.run(cmdline,stdout=subprocess.PIPE)
- putlocal(myip,'toreset','yes')
- putlocal(myip,'configured','no')
- with open('/root/evacuatelocal','a') as f:
-  f.write('iamknown '+myip+' '+name+'\n')
- with open('/root/evacuatelocal','a') as f:
-  f.write('rebooting '+myip+' '+name+'\n')
- cmdline=['/TopStor/rebootme','finished',bargs[-3]]
- result=subprocess.run(cmdline,stdout=subprocess.PIPE)
- return 1
+ thehosts=get('toremove','0')
+ leader=get('leader','--prefix')[0][0].replace('leader/','')
+ myhost=hostname()
+ myip=get('ready',myhost)[0][1]
+ print(myip,myhost,leader,str(thehosts))
+ for host in thehosts:
+  hostn=host[0].replace('toremove/','')
+  if myhost in hostn and myhost in leader:
+   cmdline=['/TopStor/Converttolocal.sh',myip]
+   result=subprocess.run(cmdline,stdout=subprocess.PIPE)
+  if myhost in hostn and myhost not in leader:
+   putlocal(myip,'toreset','yes')
+   put('toremove/'+hostn+'/'+myhost,'done')
+   cmdline=['/TopStor/rebootme','finished']
+   result=subprocess.run(cmdline,stdout=subprocess.PIPE)
+  if myhost not in hostn : 
+   hosts=get('toremove/'+hostn,'done')
+   if myhost not in str(hosts):
+    put('toremove/'+hostn+'/'+myhost,'done')
+    cmdline=['/pace/removetargetdisks.sh', hostn]
+    result=subprocess.run(cmdline,stdout=subprocess.PIPE)
+  if myhost not in hostn and myhost in leader:
+   actives=get('Active','--prefix')
+   dones=get('toremove/'+hostn,'done')
+   doneall=1
+   for active in actives:
+    activen=active[0].replace('ActivePartners/','')
+    if activen not in str(dones) and activen not in str(thehosts):
+     print(activen,str(dones),str(thehosts))
+     doneall=0
+     break
+   if doneall==1:
+    deli("", hostn)
+    put('tosync','yes')
+    logmsg.sendlog('Evacuaesu01','info','system',hostn)
+ return
 
 if __name__=='__main__':
  setall(*sys.argv[1:])
