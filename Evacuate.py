9abe88d4484305e910b6dcdaeedb78b93cdc9573
dkjlfsadl@sldkfj.com <dkjlfsadl@sldkfj.com>
2020-07-25 15:56:46 +0300

    dfja;

--- Evacuate.py
+++ Evacuate.py
@@ -5,7 +5,7 @@ from etcdput import etcdput as put
 import logmsg
 def do(*args):
  logmsg.sendlog('Evacuaest01','info',args[-1],args[-2])
- put('toremovenode/'+args[-2],'start')
+ put('toremove/'+args[-2],'start')
  put("tosync","yes")
 
 if __name__=='__main__':
