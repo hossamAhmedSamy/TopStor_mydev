b6be39c0b1c6a235c3cafe88b0c132627c2c47a7
dlksj.ewjf <dklljfd@sldjf.com>
2021-08-21 15:18:34 +0300

    fixing

--- Evacuate.py
+++ Evacuate.py
@@ -1,7 +1,9 @@
 #!/bin/python3.6
 import subprocess,sys, datetime
+from time import time
 from logqueue import queuethis
 from etcdput import etcdput as put 
+from broadcasttolocal import broadcasttolocal as broadcasttolocal 
 from etcdget import etcdget as get 
 import logmsg
 def do(*args):
@@ -10,9 +12,14 @@ def do(*args):
  if '1' in perfmon:
   queuethis('Evacuate','toremove',args[-1])
  logmsg.sendlog('Evacuaest01','info',args[-1],args[-2])
+ evacip = get('ActivePartners/'+args[-2])
  put('toremove/'+args[-2],'start')
  put("tosync","yes")
- get('to', '--prefix')
+ stamp = time()
+ put('sync/evacuatehost', stamp)
+ put('modified/evacuatehost/'+args[-2], evacip)
+ broadcasttolocal('sync/evacuatehost/', stamp)
+ broadcasttolocal('modified/evacuatehost/'+args[-2], evacip)
 
 if __name__=='__main__':
  do(*sys.argv[1:])
