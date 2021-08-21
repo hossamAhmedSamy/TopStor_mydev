45b832416776c035e3f3e00026e28ab9488859b0
dlksj.ewjf <dklljfd@sldjf.com>
2021-08-21 20:37:10 +0300

    fixing

--- factory.sh
+++ factory.sh
@@ -3,5 +3,5 @@ cd /TopStor
 rm -rf key/adminfixed.gpg && cp factory/adminfixed.gpg key/adminfixed.gpg
 newadmin=`cat /TopStor/factory/adminfixed.gpg`
 ./etcdput.py usershash/admin $newadmin
-./etcddel.py userhashadm/admin --prefix
+./etcddel.py usershashadm/admin --prefix
 /TopStor/autoGenPatch
