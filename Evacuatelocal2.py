305e1099bfa722ded2dd7ea3edd3124a697f6f8f
dlksj.ewjf <dklljfd@sldjf.com>
2021-08-21 20:02:48 +0300

    fixing

--- Evacuatelocal2.py
+++ Evacuatelocal2.py
@@ -37,36 +37,10 @@ def setall(*bargs):
     cmdline=['/TopStor/rebootme','reset']
     result=subprocess.run(cmdline,stdout=subprocess.PIPE)
     sleep(10)
-   
-   
-  #elif hostn not in str(get('ready','--prefix')) and hostn not in str(get('lost','--prefix')) and myhost not in hostn and myhost in leader and hostn not in str(get('possible','--prefix')):
-  elif myhost not in hostn and myhost in leader:
-   put('toremovereset/'+hostn,'reset')
-  hostreset=get('toremovereset/'+hostn,'reset')[0]
-  if hostn in str(hostreset): 
-   if myhost not in hostn : 
-    hosts=get('toremove/'+hostn,'done')
-    if myhost not in str(hosts):
-     put('toremove/'+hostn+'/'+myhost,'done')
+  if myhost not in hostn : 
      cmdline=['/pace/removetargetdisks.sh', hostn, hostip]
      result=subprocess.run(cmdline,stdout=subprocess.PIPE)
-   if myhost not in hostn and myhost in leader:
-    actives=get('Active','--prefix')
-    dones=get('toremove/'+hostn,'done')
-    doneall=1
-    for active in actives:
-     activen=active[0].replace('ActivePartners/','')
-     if activen not in str(dones) and activen not in str(thehosts): 
-      print(activen,str(dones),str(thehosts))
-      doneall=0
-      break
-    if doneall==1:
-     frstnode=get('frstnode')[0]
-     newnode=frstnode.replace('/'+hostn,'').replace(hostn+'/','')
-     put('frstnode',newnode)
-     deli("", hostn)
-     put('tosync','yes')
-     logmsg.sendlog('Evacuaesu01','info','system',hostn)
+  logmsg.sendlog('Evacuaesu01','info','system',hostn)
  if '1' in perfmon:
   queuethis('Evacuate','stop','system')
  return
