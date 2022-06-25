a13ec1157659b7ba86827e4f5c0859ead7606d4e
dlksj.ewjf <dklljfd@sldjf.com>
2022-06-25 21:42:53 +0300

    fisldkj

--- receivekeys.sh
+++ receivekeys.sh
@@ -3,19 +3,21 @@ echo $@ > /root/receivekeys
 partner=`echo $@ | awk '{print $1}'`
 partnerip=`echo $@ | awk '{print $2}'`
 clusterip=`echo $@ | awk '{print $3}'`
-port=`echo $@ | awk '{print $4}'`
-ptype=`echo $@ | awk '{print $5}'`
+ptype=`echo $@ | awk '{print $4}'`
+port=`echo $@ | awk '{print $5}'`
 ppass=`echo $@ | awk '{print $6}'`
 keys=`echo $@ | awk '{print $7}'`
 ./etcdget.py Partner $clusterip | grep $ppass | grep $port 
 if [ $? -eq 0 ];
 then
- echo it is ok >> /root/receivekeys
+ echo it is okok >> /root/receivekeys
+ authkeys=`cat /root/.ssh/authorized_keys | grep -v $partner`
+ echo $authkeys > /root/.ssh/authorized_keys
+ echo $keys | sed 's/\_spc\_/ /g' >> /root/.ssh/authorized_keys
+ sshd=`cat /etc/ssh/sshd_config | grep -v "Port 22" | grep -v $port`
+ echo -e "$sshd" > /etc/ssh/sshd_config
+ echo -e "Port $port \r"  >> /etc/ssh/sshd_config
+ echo -e "Port 22 \r" >> /etc/ssh/sshd_config
+ systemctl restart sshd
+ chmod 004 /root/.ssh/authorized_keys
 fi
-exit
-cd /TopStordata/
-mkdir ${partner}_keys 2>/dev/null
-cd ${partner}_keys 
-rm -rf * 2>/dev/null
-ssh-keygen -f $partner -N "" 1>/dev/null
-cat ${partner}.pub
