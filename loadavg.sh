a1be4f293cd3379b520c2b8d836aab19c64b7e2c
dlksj.ewjf <dklljfd@sldjf.com>
2022-08-30 20:50:56 +0200

    fxijg

--- loadavg.sh
+++ loadavg.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cores=`lscpu  | grep 'CPU(s):' | grep -v NUMA | awk '{print $2}'`
-loadavg=`uptime | awk '{print $NF'}`
+loadavg=`uptime | awk -F'average:' '{print $2}'  | awk -F',' '{print $1}' `
 echo $cores $loadavg
