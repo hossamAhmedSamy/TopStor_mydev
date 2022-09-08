305e5bf31ab903bc768d13390ed42cb28fe6d0f9
dlksj.ewjf <dklljfd@sldjf.com>
2022-09-08 23:10:25 +0300

    fljkf

--- levelthis.py
+++ levelthis.py
@@ -3,15 +3,18 @@ import sys
 
 def levelthis(fig,power='G'):
  leveldict = {'B':1, 'K':1024, 'M':1048576, 'G':1073741824,'T':1099511627776}
- try:
-  x = float(fig[:-1])
- except:
-  fig = 0
  level = str(fig).replace(',','').upper().replace('MB','M').replace('GB','G').replace('KB','K').replace('TB','T')
+
  if level[-1] in leveldict:
-  num = float(level[:-1])*leveldict[level[-1]]/leveldict[power]
+  try:
+   num = float("".join(level[:-1]))*leveldict[level[-1]]/leveldict[power]
+  except:
+   num = 0
  else:
+  try:
    num = float(level)/leveldict[power]
+  except:
+   num = 0.0 
  return num 
 
 if __name__=='__main__':
