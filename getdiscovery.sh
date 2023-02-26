ed82c4bcb1014be04381d9255c7e09cfdde0e8e7
sdlfkj <dkdjkl@dljkf.com>
2023-02-26 13:07:54 +0300

    fxiing

---
+++ getdiscovery.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/sh
+etcd='10.11.11.253'
+cd /TopStor
+rm -rf /root/discovery
+mkdir /root/discovery
+
+nmcli conn mod cmynode +ipv4.addresses $etcd/24
+nmcli conn up cmynode
+rm -rf /TopStordata/discovery.sh
+cp /TopStor/discovery.sh /TopStordata/
+sed -i 's/SLEEP/sleep 10/g' /TopStordata/discovery.sh
+leaderip=`docker exec etcdclient /TopStor/etcdgetlocal.py leaderip`
+echo starting etcd 
+docker run  --rm --name discovery --hostname discovery -v /etc/localtime:/etc/localtime:ro -v /root/gitrepo/resolv.conf:/etc/resolv.conf -p $etcd:2379:2379 -v /TopStor/:/TopStor -v /root/discovery:/default.etcd -v /TopStordata/discovery.sh:/runme.sh --net bridge0 moataznegm/quickstor:etcd  &
+docker exec intdns nslookup discovery | grep Address | grep -v 127 | awk '{print $2}'
+newip=`docker exec intdns nslookup discovery | grep Address | grep -v 127 | awk '{print $2}'`
+echo newip=$newip
+docker rm -f discovery
+rm -rf /TopStordata/discovery.sh
+cp /TopStor/discovery.sh /TopStordata/
+sed -i 's/SLEEP//g' /TopStordata/discovery.sh
+sed -i "s/ETCDIP/$newip/g" /TopStordata/discovery.sh
+docker run  -itd --rm --name discovery --hostname discovery -v /etc/localtime:/etc/localtime:ro -v /root/gitrepo/resolv.conf:/etc/resolv.conf -p $etcd:2379:2379 -v /TopStor/:/TopStor -v /root/discovery:/default.etcd -v /TopStordata/discovery.sh:/runme.sh --net bridge0 moataznegm/quickstor:etcd
+while true
+do
+	lines=`/TopStor/etcdget.py $etcd possible --prefix`
+	echo $lines
+	lines=`/TopStor/etcdget.py $etcd tostop`'s'
+	echo /pace/syncpossibles.py $leaderip $etcd
+	echo $lines | grep yes
+	if [ $? -eq 0 ];
+	then
+		break
+	fi
+	sleep 5 
+
+done
+docker rm -f discovery
+nmcli conn mod cmynode -ipv4.addresses $etcd/24
+nmcli conn up cmynode
