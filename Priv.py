91ab7dba8b6fd292d833868606267eb62f019780
sdlfkj <dkdjkl@dljkf.com>
2023-04-11 01:18:04 +0200

    fdis

--- Priv.py
+++ Priv.py
@@ -1,24 +1,35 @@
 #!/usr/bin/python3
 import sys, subprocess
-from etcdgetlocalpy import etcdget as get
+from etcdget import etcdget as get
 from etcdput import etcdput as put
 from time import time as stamp
 import logmsg
 
-myhost = get('clusternode')[0]
-leaderip = get('leaderip')[0]
-logmsg.initlog(leaderip,myhost)
-def changepriv(user,priv,request='admin'):
+def dosync(leader, leaderip, sync, *args):
+  global leaderip, leader
+  dels(leaderip, sync)
+  put(leaderip, *args)
+  put(leaderip, args[0]+'/'+leader,args[1])
+  return 
+
+
+def changepriv(leader, leaderip, myhost, myip, user,priv,request='admin'):
+ logmsg.initlog(leaderip,myhost)
+ if leader == myhost:
+    etcdip = leaderip
+ else:
+    etcdip = myip
  syspriv = 'UserPrivilegesch'
  cmdline = ['/TopStor/privthis.sh',syspriv, request]
  res = subprocess.run(cmdline,stdout=subprocess.PIPE).stdout
  if 'true' not in res.decode('utf-8'):
   return
  logmsg.sendlog('Priv1002 ','info',request,user)
- userinfo = get('usersinfo/'+user)
+ userinfo = get(etcdip,'usersinfo/'+user)
  leftpart = userinfo[0].split('/')[0:4]
  put(leaderip, 'usersinfo/'+user, '/'.join(leftpart)+'/'+priv)
- put(leaderip, 'user/'+myhost,str(stamp()))
+ dosync(leader, leaderip, 'sync/priv/'+user,'sync/priv/'+user, 'priv_'+str(stamp()))
+ #put(leaderip, 'user/'+myhost,str(stamp()))
  #broadcasttolocal('usersinfo/'+user, '/'.join(leftpart)+'/'+priv)
  print('/'.join(leftpart)+'/'+priv)
  logmsg.sendlog('Priv1003','info',request,user)
