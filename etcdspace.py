71415043ccd6c790192ee9a63915065d89570d58
sdlfkj <dkdjkl@dljkf.com>
2023-04-15 01:46:02 +0300

    fixing

--- etcdspace.py
+++ etcdspace.py
@@ -4,60 +4,31 @@ import json
 from ast import literal_eval as mtuple
 from time import sleep
 
-def space(*args):
+def space(etcd):
  os.environ['ETCDCTL_API']= '3'
  endpoints=''
- data=json.load(open('/pacedata/runningetcdnodes.txt'));
- for x in data['members']:
-  endpoints=endpoints+str(x['clientURLs'])[2:][:-2]+','
- endpoints = endpoints[:-1]
- cmdline=['/bin/etcdctl','--user=root:YN-Password_123','--endpoints='+endpoints,'--write-out=table','endpoint','status']
- err = 2
- while err == 2:
-  result=subprocess.run(cmdline,stdout=subprocess.PIPE).stdout.decode('utf-8')
-  result=subprocess.run(cmdline,stdout=subprocess.PIPE)
-  err = result.returncode
-  result=result.stdout.decode('utf-8')
-  if err == 2:
-   sleep(2)
- print(result)
- cmdline=['/bin/etcdctl','--user=root:YN-Password_123','--endpoints='+endpoints,'--write-out=json','endpoint','status']
- err = 2
- while err == 2:
-  result=subprocess.run(cmdline,stdout=subprocess.PIPE)
-  err = result.returncode
-  result=result.stdout.decode('utf-8')
-  if err == 2:
-   sleep(2)
- res = mtuple(result)[0]
+ #cmdline=['/bin/etcdctl','--user=root:YN-Password_123','--endpoints='+endpoints,'--write-out=table','endpoint','status']
+ #cmdline=['etcdctl','--user=root:YN-Password_123','--endpoints=http://'+etcd+':2379','get',key,prefix]
+ cmdline=['etcdctl','--endpoints=http://'+etcd+':2379','--write-out=table','endpoint','status']
+ result=subprocess.run(cmdline,stdout=subprocess.PIPE)
+ print(result.stdout.decode())
+ cmdline=['etcdctl','--endpoints=http://'+etcd+':2379','--write-out=json','endpoint','status']
+ result=subprocess.run(cmdline,stdout=subprocess.PIPE)
+ print(result.stdout.decode())
+ res = mtuple(result.stdout.decode())[0]
  print(res['Status']['header']['revision'])
- cmdline=['/bin/etcdctl','--user=root:YN-Password_123','--endpoints='+endpoints,'compact',str(res['Status']['header']['revision'])]
- err = 2
- while err == 2:
-  result=subprocess.run(cmdline,stdout=subprocess.PIPE)
-  err = result.returncode
-  result=result.stdout.decode('utf-8')
-  if err == 2:
-   sleep(2)
- print(result)
- cmdline=['/bin/etcdctl','--user=root:YN-Password_123','--endpoints='+endpoints,'defrag']
- err = 2
- while err == 2:
-  result=subprocess.run(cmdline,stdout=subprocess.PIPE)
-  err = result.returncode
-  result=result.stdout.decode('utf-8')
-  if err == 2:
-   sleep(2)
- print(result)
- cmdline=['/bin/etcdctl','--user=root:YN-Password_123','--endpoints='+endpoints,'--write-out=table','endpoint','status']
- err = 2
- while err == 2:
-  result=subprocess.run(cmdline,stdout=subprocess.PIPE)
-  err = result.returncode
-  result=result.stdout.decode('utf-8')
-  if err == 2:
-   sleep(2)
- print(result)
- 
+ #cmdline=['/bin/etcdctl','--user=root:YN-Password_123','--endpoints='+endpoints,'compact',str(res['Status']['header']['revision'])]
+ cmdline=['etcdctl','--endpoints=http://'+etcd+':2379','compact',str(res['Status']['header']['revision'])]
+ result=subprocess.run(cmdline,stdout=subprocess.PIPE)
+ print(result.stdout.decode())
+ #cmdline=['/bin/etcdctl','--user=root:YN-Password_123','--endpoints='+endpoints,'defrag']
+ cmdline=['etcdctl','--endpoints=http://'+etcd+':2379','defrag']
+ result=subprocess.run(cmdline,stdout=subprocess.PIPE)
+ print(result.stdout.decode())
+ #cmdline=['/bin/etcdctl','--user=root:YN-Password_123','--endpoints='+endpoints,'--write-out=table','endpoint','status']
+ cmdline=['etcdctl','--endpoints=http://'+etcd+':2379','--write-out=table','endpoint','status']
+ result=subprocess.run(cmdline,stdout=subprocess.PIPE)
+ print(result.stdout.decode())
+ return 
 if __name__=='__main__':
  space(*sys.argv[1:])
