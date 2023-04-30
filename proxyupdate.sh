aa470a96b35e7bf35173c92edfc23ca504de0368
sdlfkj <dkdjkl@dljkf.com>
2023-04-30 12:43:59 +0300

    fixing

---
+++ proxyupdate.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/sh
+fnupdate () {
+	git checkout -b $1
+	git checkout $1
+	git reset --hard
+	git clean -f
+	origin=`git remote -v | grep 252 | awk '{print $1}'`
+	remote=`git remote -v | grep github | awk '{print $1}'`
+	git pull $remote $1
+	if [ $? -ne 0 ];
+	then
+		echo something went wrong while pulling from remote $remote, branch: $1 .... consult the devleloper
+		exit
+	fi
+	git push $origin $1
+	if [ $? -ne 0 ];
+	then
+		echo something went wrong while pushing to origin: $origin, branch: $1 .... consult the devleloper
+		exit
+	fi
+}
+cjobs=(`echo TopStor pace topstorweb`)
+branch=$1
+branchc=`echo $branch | wc -c`
+if [ $branchc -le 3 ];
+then
+	echo no valid branch is supplied .... exiting
+	exit
+fi 
+flag=1
+while [ $flag -ne 0 ];
+do
+	rjobs=(`echo "${cjobs[@]}"`)
+	echo rjobs=${rjobs[@]}
+	for job in "${rjobs[@]}";
+	do
+		echo '###########################################'
+ 		echo $job
+		isexit=1
+		cd /$job
+		if [ $? -ne 0 ];
+		then
+			echo $job | grep topstorweb
+			if [ $? -eq 0 ];
+			then
+				cd /var/www/html/des20/
+				if [ $? -eq 0 ];
+				then
+					isexit=0
+				fi
+			if
+			if [ $isexit -eq 1 ];
+			then
+				echo the directory $job is not found... exiting
+				exit
+			fi
+		fi
+		fnupdate $branch 
+		cjobs=(`echo "${cjobs[@]}" | sed "s/$job//g" `)
+  	done
+	lencjobs=`echo $cjobs | wc -c`
+	if [ $lencjobs -le 3 ];
+	then
+		flag=0
+	fi
+done
+cd /TopStor
+echo finished
