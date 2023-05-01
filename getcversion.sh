2570797102171dc020838dd7b65d5897231fb366
sdlfkj <dkdjkl@dljkf.com>
2023-05-01 16:30:06 +0300

    fixing

--- getcversion.sh
+++ getcversion.sh
@@ -6,6 +6,7 @@ leader=`echo $@ | awk '{print $2}'`
 myhost=`echo $@ | awk '{print $3}'`
 version=`git branch | grep '*' | awk '{print $2}'`
 commit=`git show --abbrev-commit | grep commit | head -1 | awk '{print $2}'`
+stamp=`date +%s`
 ./etcdput.py $leaderip cversion/$myhost $version-$commit
-./etcdput.py $leaderip sync/cversion/_____/request $version-$commit
-./etcdput.py $leaderip sync/cversion/_____/request/$leader $version-$commit
+./etcdput.py $leaderip sync/cversion/_____/request cversion_$stamp 
+./etcdput.py $leaderip sync/cversion/_____/request/$leader cversion_$stamp 
