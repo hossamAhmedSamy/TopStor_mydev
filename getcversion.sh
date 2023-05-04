69ae22c1d7afb59542508b32b08b9754c6583899
sdlfkj <dkdjkl@dljkf.com>
2023-05-04 12:59:06 +0300

    fixing

--- getcversion.sh
+++ getcversion.sh
@@ -6,7 +6,13 @@ leader=`echo $@ | awk '{print $2}'`
 myhost=`echo $@ | awk '{print $3}'`
 version=`git branch | grep '*' | awk '{print $2}'`
 commit=`git show --abbrev-commit | grep commit | head -1 | awk '{print $2}'`
-stamp=`date +%s`
-./etcdput.py $leaderip cversion/$myhost $version-$commit
-./etcdput.py $leaderip sync/cversion/_____/request cversion_$stamp 
-./etcdput.py $leaderip sync/cversion/_____/request/$leader cversion_$stamp 
+oldversion=`/TopStor/etcdget.py $leaderip cversion/$myhost`
+echo $oldversion | grep $version-$commit
+if [ $? -ne 0 ];
+then
+	echo asking to sync
+	stamp=`date +%s`
+	./etcdput.py $leaderip cversion/$myhost $version-$commit
+	./etcdput.py $leaderip sync/cversion/_____/request cversion_$stamp 
+	./etcdput.py $leaderip sync/cversion/_____/request/$leader cversion_$stamp 
+fi
